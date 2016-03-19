84991303fcb37b93b00580333a83070211020840
SuperStarPL <f1time@o2.pl>
2016-03-19 08:58:58 +0100

    menu1

---
+++ Assets/Scripts/ApplicationQuit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ApplicationQuit : MonoBehaviour
+{
+    // Use this for initialization
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+    // Use this for initialization
+    public void GuzikStart()
+    {
+        SceneManager.LoadSceneAsync("OverWorld01Load");
+    }
+}
